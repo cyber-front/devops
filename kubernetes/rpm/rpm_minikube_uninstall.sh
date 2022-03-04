8ee9c155fe950f0099f07c86ce6d0a35969d8f5c
Cybernetic Frontiers LLC <cybernetic.frontiers@gmail.com>
2022-03-04 00:40:17 -0500

    edits to rpm minikube uninstaller

--- kubernetes/rpm/rpm_minikube_uninstall.sh
+++ kubernetes/rpm/rpm_minikube_uninstall.sh
@@ -2,6 +2,6 @@
 
 # This script uninstalls kubectl and minikube from a Red Hat derived Linux host
 
-apt remove kubectl -y
+yum remove kubectl -y
 dpkg --remove minikube
 rm -f /etc/yum.repos.d/kubernetes.repo
